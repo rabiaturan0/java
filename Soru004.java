9aa19c10183e02adb1a4408460e6a1af01cb6c02
rabiaturan0 <rbtrn229@gmail.com>
2023-01-07 00:15:02 +0300

    7/01/23

---
+++ Soru004.java
@@ -0,0 +1,26 @@
+package FatmaSorular;
+
+import java.util.Scanner;
+
+public class Soru004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir numara giriniz.");
+        int lineNumber = input.nextInt();
+        int i, j, k, number;
+
+        for(i = 0; i<lineNumber; i++)
+        {
+            for(k=lineNumber; k>i; k--)
+                System.out.print(" ");
+            number = 1;
+
+            for (j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ Soru005.java
@@ -0,0 +1,16 @@
+package FatmaSorular;
+
+public class Soru005 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            for (int k = i; k < 10; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+            for (int boss = 1; boss < i + 1; boss++)
+                System.out.print(" ");
+        }
+
+    }
+}
